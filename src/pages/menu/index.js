23a42365fd4cbd111b4c07fccceb9ec511dba426
FGEGitHub <pipao.pipo@gmail.com>
2024-04-26 17:43:27 -0300

    foot

--- src/pages/menu/index.js
+++ src/pages/menu/index.js
@@ -4,7 +4,18 @@ import Tarjeta from '../../components/inicio/tarjetas';
 import Nav from '../../components/inicio/nav';
 import backgroundImage1 from "../../Assets/imagenfondo1.jpg"; // Importa tus imágenes de fondo
 import backgroundImage2 from "../../Assets/imagenfondo1.jpg"; 
-
+import CssBaseline from '@mui/material/CssBaseline';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import AddIcon from '@mui/icons-material/Add';
+import SearchIcon from '@mui/icons-material/Search';
+import MoreIcon from '@mui/icons-material/MoreVert';
+import { styled } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Fab from '@mui/material/Fab';
 const backgroundOverlayStyle = {
   position: 'fixed',
   top: 0,
@@ -15,7 +26,15 @@ const backgroundOverlayStyle = {
   display: 'flex',
   flexDirection: 'column',
 };
-
+const StyledFab = styled(Fab)({
+    position: 'absolute',
+    zIndex: 1,
+    top: -30,
+    left: 0,
+    right: 0,
+    margin: '0 auto',
+  });
+  
 const backgroundOverlayImageStyle = {
   flex: '1 1 auto',
   backgroundSize: 'cover',
@@ -69,7 +88,23 @@ export default function Paginas() {
                     <Tarjeta />
                     <Tarjeta />
                     {/* Agrega más componentes aquí */}
-               
+                    <AppBar position="fixed" color="primary" sx={{ top: 'auto', bottom: 0 }}>
+        <Toolbar sx={{ backgroundColor: '#1a393c' }}>
+          <IconButton color="inherit" aria-label="open drawer">
+            <MenuIcon />
+          </IconButton>
+          <StyledFab color="secondary" aria-label="add">
+            <AddIcon />
+          </StyledFab>
+          <Box sx={{ flexGrow: 1 }} />
+          <IconButton color="inherit">
+            <SearchIcon />
+          </IconButton>
+          <IconButton color="inherit">
+            <MoreIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
             </div>
         </>
     );
