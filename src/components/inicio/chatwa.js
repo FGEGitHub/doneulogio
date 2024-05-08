a566680b90479a8b23456c804df68f2668945a01
MaximilianoBury <mbury@legalario.com>
2024-05-08 11:53:26 -0300

    actualizacion de colores segun identidad d.Eulogio

--- src/components/inicio/chatwa.js
+++ src/components/inicio/chatwa.js
@@ -7,31 +7,33 @@ import logo from "../../Assets/doneu1.png";
 const Whatsapp = () => {
 	return (
 		<WhatsAppWidget
-			phoneNo="5493794781818"
-			position="right"
-			widgetWidth="300px"
-			widgetWidthMobile="260px"
-			autoOpen={true}
-			autoOpenTimer={5000}
-			messageBox={true}
-			messageBoxTxt="Quiero consultar el precio del lote N°"
-			iconSize="70"
-			iconColor="#00A313"
-			iconBgColor="#green"
-			headerIcon={logo}
-			headerIconColor="pink"
-			headerTxtColor="white"
-			headerBgColor="#89DA59"
-			headerTitle="Doble D"
-			headerCaption="Online"
-			bodyBgColor="#80BD9E"
-			chatPersonName="Suporte"
-			chatMessage={<>Hola 👋 <br /><br /> En que te puedo ayudar?</>}
-			footerBgColor="#fff"
-			btnBgColor="springgreen"
-			btnTxtColor="black"
-			btnTxt="Comenzar Chat"
-		/>
+    phoneNo="5493794781818"
+    position="right"
+    widgetWidth="300px"
+    widgetWidthMobile="260px"
+    autoOpen={true}
+    autoOpenTimer={5000}
+    messageBox={true}
+    messageBoxTxt="Quiero consultar el precio del lot..."
+    iconSize="60"
+    iconColor="#8eb937" //armonia y estabilidad de identidad don eulogio
+    iconBgColor="black"
+    headerIcon={logo}
+    headerIconColor="pink"
+    headerTxtColor="white"
+    headerBgColor="#1a393c" // Fondo del encabezado en el color #1a393c
+    bodyBgColor="#f5f5f5" // Fondo del cuerpo del chat en un gris claro (#f5f5f5)
+    headerTitle="Don Eulogio"
+	headerCaption="Online"
+    chatPersonName="Soporte"
+    chatMessage={<>Hola 👋 <br /><br /> En qué puedo ayudarte?</>}
+    footerBgColor="#999" // Pie de página en un tono neutro (#999)
+    btnBgColor="#8eb937" // Botón "Comenzar Chat" en verde primavera
+    btnTxtColor="black"
+    btnTxt="Comenzar Chat"
+    placeholder="Escribe tu mensaje"
+/>
+       
 	);
 };
 
