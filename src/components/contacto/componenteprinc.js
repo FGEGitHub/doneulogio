313290b3c63e576f3bfa53aa9bca2cd48e3ce9e7
FGEGitHub <pipao.pipo@gmail.com>
2024-10-09 12:55:51 -0300

    contacto

--- src/components/contacto/componenteprinc.js
+++ src/components/contacto/componenteprinc.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Container, Typography, Box, CssBaseline, AppBar, Toolbar, Link } from '@mui/material';
-import foto1 from '../../Assets/urabierta.png';
+import foto1 from '../../Assets/contactanos.png';
+import foto2 from '../../Assets/mapacontactanos.png';
+import DYC from '../../Assets/dyc.png';
 import Formulario from './formulario';
 import logo from '../../Assets/logonav2.png';
 import '../urbanizaciona/UrbanizacionAbierta.css';
@@ -12,7 +14,9 @@ const UrbanizacionAbierta = () => {
   return (
     <React.Fragment>
       <CssBaseline />
- 
+      <Toolbar className="urbanizacion-toolbar">
+          <img src={logo} alt="Logo" className="urbanizacion-logo" />
+        </Toolbar>
 
       <Container>
       <nav className="urbanizacion-nav">
@@ -103,13 +107,30 @@ const UrbanizacionAbierta = () => {
   </div>
 </nav>
 
-        <img src={foto1} alt="Urbanización Abierta" className="urbanizacion-header-image" />
+<div style={styles.imageContainer}>
+      <img 
+        src={foto1} 
+        alt="Urbanización Abierta" 
+        style={styles.image1} 
+      />
+      <span style={styles.imageText}>  Contactanos</span>
+    </div>
         <br /><br />
 
         <Formulario />
 
       </Container>
-
+      
+      <img 
+        src={DYC} 
+        alt="Urbanización Abierta" 
+        style={styles.imagealt} 
+      />
+      <img 
+        src={foto2} 
+        alt="Urbanización Abierta" 
+        style={styles.image} 
+      />
       <Box className="urbanizacion-footer">
         <Typography variant="body1">&copy; Don Eulogio - Todos los derechos reservados</Typography>
       </Box>
@@ -117,4 +138,41 @@ const UrbanizacionAbierta = () => {
   );
 };
 
+const styles = {
+  imageContainer: {
+    position: 'relative',
+    display: 'inline-block', // Ajusta el tamaño al de la imagen
+  },
+  image: {////imagen del mapa
+    marginTop:'5%',
+    width: '100%',
+    height: 'auto',
+    display: 'block',
+  },
+  image1: {
+    ///imagen de arriba
+    width: '100%',
+    height: 'auto',
+    display: 'block',
+  },
+  imagealt: {///imagen de los logos
+    marginTop:'5%',
+    marginButton:'10%',
+    width: '30%',
+    height: 'auto',
+    display: 'block',
+    margin: '0 auto', /* Centraliza horizontalmente */
+  },
+  imageText: {
+    position: 'absolute',
+    top: '50%',
+    left: '10%', // Mueve el texto más a la izquierda
+    transform: 'translate(0, -50%)', // Mueve solo verticalmente para que no se afecte la posición horizontal
+    color: 'white', // El color del texto
+    fontSize: '60px', // Texto más grande
+    fontWeight: 'bold', // Hacer el texto en negrita
+    textShadow: '2px 2px 4px rgba(0, 0, 0, 0.7)', // Sombra para que se lea mejor
+    pointerEvents: 'none', // Para que el texto no interfiera con la imagen en términos de interacción
+  },
+};
 export default UrbanizacionAbierta;
