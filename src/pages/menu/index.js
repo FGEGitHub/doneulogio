381db169dec1efd9d3fda7aaf26477b685dad101
FGEGitHub <pipao.pipo@gmail.com>
2024-11-01 20:25:33 -0300

    dasxa

--- src/pages/menu/index.js
+++ src/pages/menu/index.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import fondoMaster from "../../Assets/fondomaster.png"; // Importa la imagen de fondo
 import General from '../../components/inicio2/Component';
+import Mobile from '../../components/iniciomobile/componete1';
+import Nav from '../../components/inicio/nav';
 
 const backgroundOverlayStyle = {
   position: 'fixed',
@@ -35,6 +37,19 @@ const generalContainerStyle = {
 
 export default function Paginas() {
     const [showSecondBackground, setShowSecondBackground] = useState(false);
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        // Función que verifica si el ancho de la pantalla es menor o igual a 768px
+        const handleResize = () => {
+          setIsMobile(window.matchMedia("(max-width: 768px)").matches);
+        };
+    
+        // Llamamos a la función al montar el componente y cada vez que la ventana cambia de tamaño
+        handleResize();
+        window.addEventListener('resize', handleResize)
+ } )
+        
 
     useEffect(() => {
         const handleScroll = () => {
@@ -63,9 +78,9 @@ export default function Paginas() {
          
             {/* Contenido principal */}
         
-        
+            {isMobile ? <div style={{  padding: '-10px', textAlign: 'left' }}><Nav/><Mobile /></div> :       <div style={{  padding: '10px', textAlign: 'center' }}><General />  </div>}
               
-                    <General />
+                
              
            </>
     );
