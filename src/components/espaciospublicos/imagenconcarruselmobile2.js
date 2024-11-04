504645e9d83c6bf6752b17a866df96ddc3252cc2
FGEGitHub <pipao.pipo@gmail.com>
2024-11-04 18:14:53 -0300

    carrusel

--- src/components/espaciospublicos/imagenconcarruselmobile2.js
+++ src/components/espaciospublicos/imagenconcarruselmobile2.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Slider from 'react-slick';
 import carrusel1 from '../../Assets/carrusel2.png';
 import carrusel2 from '../../Assets/carrusel2.png';
-import carrusel3 from '../../Assets/carrusel2.svg';
+import carrusel3 from '../../Assets/carrusel2.png';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 
